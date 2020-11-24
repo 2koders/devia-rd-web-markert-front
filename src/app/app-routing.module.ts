7e7a7f6e51bcdebc8e6ce3701e1a8838d48ecf6f
Enmanuel Balcacer <enmah4k@gmail.com>
2020-11-24 16:44:10 -0400

    Paginas home y market/home creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/landing/home/home.component';
+import { IndexComponent } from './pages/market/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+     component: HomeComponent, 
+    children: [{
+      // path: ''
+    }]
+  },
+  {
+    path: 'market', 
+    component: IndexComponent, 
+    children: [{
+      // path: ''
+    }]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
